66998262bc5042b20d06002e4ba35cef961fc42e
Erik Alfvin <erikalfvin@gmail.com>
2020-03-28 11:18:23 -0500

    Stashed minor changes

--- controllers/art_controller.js
+++ controllers/art_controller.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
-router.post("/", function(req, res) {
-    console.log(req.body.pieces);
-    res.render("search", {artPieces: req.body});
-});
-
 router.get("/", function(req, res) {
     return fs.readFile(__dirname + "/../views/index.html", function(err, data) {
         if (err) throw err;
