4f6d268f2196fe470e2cd6b0c6b58cce5126ed40
Erik Alfvin <erikalfvin@gmail.com>
2020-03-28 11:24:12 -0500

    stashed chanhes

--- controllers/art_controller.js
+++ controllers/art_controller.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", function(req, res) {
-    console.log(req.body.pieces);
-    res.render("search", {artPieces: req.body.pieces});
-});
-
 router.get("/", function(req, res) {
     console.log(req.body);
     res.render("search", {artPieces: null});
