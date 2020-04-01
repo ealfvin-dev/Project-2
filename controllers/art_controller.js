643c1bd304067124f6e4397479240de2b3ce473f
Erik Alfvin <erikalfvin@gmail.com>
2020-04-01 18:48:50 -0500

    Added get route to gallery

--- controllers/art_controller.js
+++ controllers/art_controller.js
@@ -4,30 +4,39 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-app.get("/", function(req, res) {
-    return fs.readFile(__dirname + "/../public/index.html", function(err, data) {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });  
-});
-
-app.get("/api/gallery", function(req, res) {
-    db.Gallery.findAll({}).then((dbGallery)=>{
-        res.json(dbGallery);
+    app.get("/", function(req, res) {
+        return fs.readFile(__dirname + "/../public/index.html", function(err, data) {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });  
     });
-});
 
-app.post("/api/collection", function(req, res) {
-    db.Collections.create(req.body).then(function(dbCollection) {
-        res.json(dbCollection);
+    app.get("/api/galleryNames", function(req, res) {
+        db.Gallery.findAll({}).then((dbGallery)=>{
+            res.json(dbGallery);
+        });
     });
-});
 
+    app.get("/galleries/:galleryName", function(req, res) {
+        db.Collections.findAll({
+            where: {
+                gallery: req.params.galleryName
+            }
+        }).then(function(results) {
+            res.json(results);
+        });
+    });
 
-app.post("/api/gallery", function(req, res) {
-    db.Gallery.create(req.body).then(function(dbGallery) {
-        res.json(dbGallery);
-    })
-})
+    app.post("/api/collection", function(req, res) {
+        db.Collections.create(req.body).then(function(dbCollection) {
+            res.json(dbCollection);
+        });
+    });
+
+    app.post("/api/gallery", function(req, res) {
+        db.Gallery.create(req.body).then(function(dbGallery) {
+            res.json(dbGallery);
+        })
+    });
 };
